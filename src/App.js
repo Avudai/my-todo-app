61ef9c8cac3865ffbc21cb58c71b4f2a3dccf0bd
root <aavudaikannan@gmail.com>
2020-11-25 10:55:39 -0800

    Added header with bootstrap libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // set the state of the application
+    this.state = {
+      userInput: "",
+      list: []
+    }
+  }
+
+  render() {
+    return (<Container>
+      <Row style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: '3rem',
+        fontWeight: 'bolder',
+      }}> TODO LIST :)
+
+      </Row>
+    </Container>)
+  }
 }
 
 export default App;
